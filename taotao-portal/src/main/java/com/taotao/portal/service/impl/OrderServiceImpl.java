0203665d234d41b7e112ba3674e586f9c2458ff3
FantasticPan <706390664@qq.com>
2018-08-28 16:44:41 +0800

    提交订单服务

---
+++ taotao-portal/src/main/java/com/taotao/portal/service/impl/OrderServiceImpl.java
@@ -0,0 +1,34 @@
+package com.taotao.portal.service.impl;
+
+import com.taotao.common.pojo.TaotaoResult;
+import com.taotao.common.util.HttpClientUtil;
+import com.taotao.common.util.JsonUtil;
+import com.taotao.portal.pojo.Order;
+import com.taotao.portal.service.OrderService;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+/**
+ * Created by FantasticPan on 2018/8/27.
+ */
+@Service
+public class OrderServiceImpl implements OrderService {
+
+    @Value("${ORDER_BASE_URL}")
+    private String ORDER_BASE_URL;
+    @Value("${ORDER_CREATE_URL}")
+    private String ORDER_CREATE_URL;
+
+    @Override
+    public String createOrder(Order order) {
+        //调用taotao-order的服务提交订单
+        String json = HttpClientUtil.doPostJson(ORDER_BASE_URL + ORDER_CREATE_URL, JsonUtil.objectToJson(order));
+        //把json装换为TaotaoResult
+        TaotaoResult taotaoResult = TaotaoResult.format(json);
+        if (taotaoResult.getStatus() == 200) {
+            Object orderId = taotaoResult.getData();
+            return orderId.toString();
+        }
+        return "";
+    }
+}
